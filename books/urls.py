ac4c43a929d703d58f0a90f7a94c09bb26cd7506
hafeez <hafeez.cs56@gmail.com>
2024-07-22 11:37:54 +0500

    22-07-2024

--- books/urls.py
+++ books/urls.py
@@ -16,6 +16,5 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('signup/', views.user_signup, name='signup'),
     path('logout/', views.user_logout, name='logout'),
-    path('send/', views.send_daily_emails, name='send'),
     
 ]
